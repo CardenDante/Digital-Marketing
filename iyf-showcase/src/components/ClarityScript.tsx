bb457d828bbba8d6679c0757c07ee64b5d736e9e
CardenDante <cardendante@gmail.com>
2025-03-14 23:30:18 +0300

    fix: update ClarityScript tag ID for improved tracking

--- iyf-showcase/src/components/ClarityScript.tsx
+++ iyf-showcase/src/components/ClarityScript.tsx
@@ -9,10 +9,10 @@ export default function ClarityScript() {
       dangerouslySetInnerHTML={{
         __html: `
           (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "qauwt5h8rx");
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "qo9d7p7xmw");
         `
       }}
     />
